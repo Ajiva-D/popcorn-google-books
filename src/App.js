ec4a64e6be55b96d7a6d11fc990e2a7d93303f61
David Aji <davidenochaji@gmail.com>
2022-02-27 10:25:06 +0100

    feat:add reading lists and book lists pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
+
 import './App.css';
+import BookLists from './pages/BookLists';
+import Header from './components/Header/Header';
+import {
+	BrowserRouter,
+	Routes,
+	Route,
+} from "react-router-dom";
+import ReadingLists from './pages/ReadingLists';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<BrowserRouter>
+		<div className="App">
+			<Header />
+	
+				<Routes>
+					<Route path="/" element={<BookLists />}/>
+					<Route path="/reading-lists" element={<ReadingLists/>}/>
+				</Routes>
+		</div>
+		</BrowserRouter>
+	);
 }
 
 export default App;
---
+++ src/pages/ReadingLists.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import BookItem from '../components/BookItem/BookItem';
+
+const ReadingLists = () => {
+	return (
+		<div>	
+			<div className='book-list-con'>
+			<BookItem />
+			<BookItem />
+			<BookItem />
+			<BookItem />
+			<BookItem />
+			<BookItem />
+			<BookItem />
+			<BookItem />
+			<BookItem />
+			<BookItem />
+		</div>
+	</div>
+	)
+}
+
+export default ReadingLists
